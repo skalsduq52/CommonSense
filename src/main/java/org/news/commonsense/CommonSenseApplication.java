8495f22f34ce8d43abac0f753266c34cd1a09887
skalsduq52 <skalsduq@naver.com>
2025-02-13 16:15:21 +0900

    커밋 초기화!

---
+++ src/main/java/org/news/commonsense/CommonSenseApplication.java
@@ -0,0 +1,13 @@
+package org.news.commonsense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonSenseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonSenseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/news/commonsense/CommonSenseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.news.commonsense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonSenseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
